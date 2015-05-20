d1de5a541ebd4e59d6e07bd57755dc34327f0e15
Colm O'Sullivan <cbpos1989@gmail.com>
2015-05-20 10:50:47 +0100

    Initial Commit

---
+++ Enigma/src/com/cbpos1989/github/enigma/EnigmaHandler.java
@@ -0,0 +1,14 @@
+package com.cbpos1989.github.enigma;
+/**
+ * 
+ */
+
+/**
+ * @author Colm O'Sullivan
+ *
+ */
+public class EnigmaHandler {
+	public static void main(String[] args){
+		System.out.println("Working");
+	}
+}
