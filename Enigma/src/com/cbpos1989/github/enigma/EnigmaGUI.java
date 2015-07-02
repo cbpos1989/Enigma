6adca5c272802bccb0dc330bc73993f073caee0e
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-02 11:57:18 +0100

    Light Label change color to responding key presses

--- Enigma/src/com/cbpos1989/github/enigma/EnigmaGUI.java
+++ Enigma/src/com/cbpos1989/github/enigma/EnigmaGUI.java
@@ -9,6 +9,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
 
 public class EnigmaGUI extends JFrame {
 
@@ -22,6 +26,8 @@ public class EnigmaGUI extends JFrame {
 	private char firstRow[] = {'Q','W','E','R','T','Z','U','I','O'};
 	private char secondRow[] = {'A','S','D','F','G','H','J','K'};
 	private char thirdRow[] = {'P','Y','X','C','V','B','N','M','L'};
+	private JLabel[] lights = new JLabel[26];
+	private int counter = 0;
 	
 
 	/**
@@ -37,14 +43,28 @@ public class EnigmaGUI extends JFrame {
 	
 	void initialiseGui(){
 		GridLayout mainLayout = new GridLayout(2,1,100,0);
-		setLayout(mainLayout);
+		getContentPane().setLayout(mainLayout);
 		
 		//Light Panel
 		JPanel lightPanel = new JPanel();
 		lightPanel.setBorder(BorderFactory.createEmptyBorder(50,400,50,50));
 		lightPanel.setLayout(new GridLayout(3,9,5,15));
+		//lightPanel.setLayout(null);
 		lightPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-		this.add(lightPanel);
+		getContentPane().add(lightPanel);
+		
+//		JLabel lblQ = new JLabel("Q");
+//		lblQ.setHorizontalAlignment(SwingConstants.CENTER);
+//		lblQ.setFont(new Font("Tahoma", Font.PLAIN, 14));
+//		lblQ.setBounds(10, 11, 25, 25);
+//		lightPanel.add(lblQ);
+//		addLightsToArray(lblQ);
+//	
+//		JLabel lblW = new JLabel("W");
+//		lblW.setHorizontalAlignment(SwingConstants.CENTER);
+//		lblW.setFont(new Font("Tahoma", Font.PLAIN, 14));
+//		lblW.setBounds(36, 11, 25, 25);
+//		lightPanel.add(lblW);
 		
 		createLights(firstRow, lightPanel);
 		createLights(secondRow, lightPanel);
@@ -54,13 +74,22 @@ public class EnigmaGUI extends JFrame {
 		JPanel keyboardPanel = new JPanel();
 		keyboardPanel.setLayout(new GridLayout(3,9,5,15));
 		keyboardPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-		this.add(keyboardPanel);
+		getContentPane().add(keyboardPanel);
 		
 		createButtons(firstRow, keyboardPanel);
 		createButtons(secondRow, keyboardPanel);
 		createButtons(thirdRow, keyboardPanel);
 		
 		setVisible(true);
+	}
+	
+	void addLightsToArray(JLabel label){
+		lights[counter] = label;
+		counter++;
+	}
+	
+	void lightUpLabel(int i, Color clr){
+		lights[i].setForeground(clr);
 	}
 	
 	void createLights(char[] chars, JPanel panel){
@@ -70,6 +99,7 @@ public class EnigmaGUI extends JFrame {
 		for (int i = 0; i < chars.length; ++i) {
 			lightRow[i] = new JLabel(chars[i] + "");
 			p.add(lightRow[i]);
+			addLightsToArray(lightRow[i]);
 		}
 		
 		panel.add(p);
@@ -97,6 +127,4 @@ public class EnigmaGUI extends JFrame {
 			b.addActionListener(eh);
 		}
 	}
-	
-	
 }
--- Enigma/src/com/cbpos1989/github/enigma/EnigmaHandler.java
+++ Enigma/src/com/cbpos1989/github/enigma/EnigmaHandler.java
@@ -1,5 +1,6 @@
 package com.cbpos1989.github.enigma;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -27,6 +28,37 @@ public class EnigmaHandler implements ActionListener{
 	}
 	
 	void keyPressed(String letter){
+		Color clr = Color.RED;
+		
+		//for(JLabel lbl: eGui.lig);
+		switch(letter){
+		case "Q": eGui.lightUpLabel(0, clr); break;
+		case "W": eGui.lightUpLabel(1,clr); break;
+		case "E": eGui.lightUpLabel(2,clr); break;
+		case "R": eGui.lightUpLabel(3,clr); break;
+		case "T": eGui.lightUpLabel(4,clr); break;
+		case "Z": eGui.lightUpLabel(5,clr); break;
+		case "U": eGui.lightUpLabel(6,clr); break;
+		case "I": eGui.lightUpLabel(7,clr); break;
+		case "O": eGui.lightUpLabel(8,clr); break;
+		case "A": eGui.lightUpLabel(9,clr); break;
+		case "S": eGui.lightUpLabel(10,clr); break;
+		case "D": eGui.lightUpLabel(11,clr); break;
+		case "F": eGui.lightUpLabel(12,clr); break;
+		case "G": eGui.lightUpLabel(13,clr); break;
+		case "H": eGui.lightUpLabel(14,clr); break;
+		case "J": eGui.lightUpLabel(15,clr); break;
+		case "K": eGui.lightUpLabel(16,clr); break;
+		case "P": eGui.lightUpLabel(17,clr); break;
+		case "Y": eGui.lightUpLabel(18,clr); break;
+		case "X": eGui.lightUpLabel(19,clr); break;
+		case "C": eGui.lightUpLabel(20,clr); break;
+		case "V": eGui.lightUpLabel(21,clr); break;
+		case "B": eGui.lightUpLabel(22,clr); break;
+		case "N": eGui.lightUpLabel(23,clr); break;
+		case "M": eGui.lightUpLabel(24,clr); break;
+		case "L": eGui.lightUpLabel(25,clr); break;
+		}
 		System.out.print(letter + " ");
 	}
 }
